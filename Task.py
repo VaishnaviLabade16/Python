9697a6177e51d99f226ae20a50a207fc5882870b
VaishnaviLabade16 <vaishnavilabade7@gmail.com>
2025-12-29 16:28:17 +0530

    Add files via upload

---
+++ Task.py
@@ -0,0 +1,72 @@
+#Practice
+'''
+a=20
+b=4
+print(a+b)
+print(b-a)
+print(a*b)
+print(a/b)
+print(a**b)
+print(a//b)
+print(a==b)
+print(a!=b)
+print(a<b)
+print(a>b)
+print(a<=b)
+print(a>=b)
+'''
+#list
+'''l=["sakshi","mayuri","karina","vaishnavi"]
+print(l)
+l.append("om")
+print(l)
+l.remove("om")
+print(l)'''
+
+#Tuple
+'''
+t=("A","B","C")
+print(t)#tuple
+l=list(t)
+print(l)#tuple into list
+l.append(10)
+print(l)
+l.remove("C")
+print(l)
+l[0]="om"
+print(l)'''
+
+#Dictionary
+'''
+d1={
+    "name" : "vaishnavi",
+    "id" : 1,
+    "city" : "pune"
+}
+print(d1)
+d["id"]=2
+print(d) #add
+d.pop("id")
+print(d) #delete
+d2={
+    "name" : "Sakshi",
+    "id" : 2,
+    "city" : "nashik"
+}
+print(d2)
+d=d1|d2
+print(d)
+'''
+
+#Set
+'''
+s1={"a","b","c","d"}
+s2={"d","e","f"}
+print(s1|s2)
+print(s1&s2)
+print(s1-s2)
+print(s1^s2)
+'''
+
+
+
