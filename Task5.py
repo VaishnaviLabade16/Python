19c82e6ca0c2674e12188cf5c1e684d854426105
VaishnaviLabade16 <vaishnavilabade7@gmail.com>
2026-01-15 13:03:00 +0530

    Add files via upload

---
+++ Task5.py
@@ -0,0 +1,39 @@
+#Square root of a number Program
+'''
+n=int(input("Enter number: "))
+x=n**0.5
+print("Answer: ",x)
+'''
+
+#Area of Rectangle
+'''
+l=float(input("Enter Lenght of Rectangle: "))
+b=float(input("Enter Breadth of Rectangle: "))
+Area=l*b
+print("Area of Rectangle: ",Area)
+'''
+
+#Area and Perimeter of Square
+'''
+s=int(input("Enter Side: "))
+Area=s*s
+perimeter=4*s
+print("Area of Square: ",Area)
+print("Perimeter of Square: ",perimeter)
+'''
+
+#Swap Values of 2 Variables
+n1=int(input("Enter number: "))
+n2=int(input("Enter number: "))
+print("*****number Before Swap*******")
+print("N1: ",n1)
+print("n2: ",n2)
+
+temp=0
+
+temp=n1
+n1=n2
+n2=temp
+print("*******After Swiping*********")
+print("N1: ",n1)
+print("N2: ",n2)
