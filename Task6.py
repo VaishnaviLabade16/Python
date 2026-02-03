c529804658585b215965b198184f5d0783aa32d7
VaishnaviLabade16 <vaishnavilabade7@gmail.com>
2026-02-03 19:08:23 +0530

    Add files via upload

---
+++ Task6.py
@@ -0,0 +1,12 @@
+# Create a class Book
+class Book:
+    # Method to display book details
+    def display(self):
+        print("Book Title: The Alchemist")
+        print("Author: Paulo Coelho")
+
+# Create an object of the class
+book1 = Book()
+
+# Call the display method
+book1.display()
