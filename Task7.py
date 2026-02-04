7ec9bae0349740b7c49ce85788a3cc0697d54ec8
VaishnaviLabade16 <vaishnavilabade7@gmail.com>
2026-02-04 20:20:51 +0530

    Add files via upload

---
+++ Task7.py
@@ -0,0 +1,10 @@
+#Create a class book with method display that print a book title 
+# and author create an object and call the method give me python code
+
+class Book:
+    def display(self):
+     print("Title = wings of fire")
+     print("Author = APJ Abdul kalam ")
+
+Book1=Book()
+Book1.display()    
