47021f4c255bdc659a28279a34232514220c829f
VaishnaviLabade16 <vaishnavilabade7@gmail.com>
2026-02-18 21:32:40 +0530

    Add files via upload

---
+++ Integer_to_Float.py
@@ -0,0 +1,7 @@
+num_i=123
+num_f=1.23
+num_new=num_i+num_f
+print("Datatype of num_i: ",type(num_i))
+print("Datatype of num_f: ",type(num_f))
+print("Value of num_new: ",num_new)
+print("Datatype of num_new: ",type(num_new))
