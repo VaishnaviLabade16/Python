03002c879757e0c66720904a970b495bc12e866d
VaishnaviLabade16 <vaishnavilabade7@gmail.com>
2026-02-27 16:40:04 +0530

    Add files via upload

---
+++ Center_Peramid.py
@@ -0,0 +1,12 @@
+n = int(input("Enter number of rows: "))
+
+for i in range(n):
+    # Print spaces
+    for j in range(n - i - 1):
+        print(" ", end="")
+
+    # Print stars
+    for k in range(2 * i + 1):
+        print("*", end="")
+
+    print()
