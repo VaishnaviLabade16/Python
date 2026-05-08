36a1b8884b3b740f4d2e82a4365eaff47847507a
VaishnaviLabade16 <vaishnavilabade7@gmail.com>
2026-05-08 18:43:42 +0530

    Add files via upload

---
+++ Gnome_Sort.py
@@ -0,0 +1,11 @@
+def gnome_sort(arr):
+    index = 0
+    while index < len(arr):
+        if index == 0 or arr[index] >= arr[index - 1]:
+            index += 1
+        else:
+            arr[index], arr[index - 1] = arr[index - 1], arr[index]
+            index -= 1
+    return arr
+
+print(gnome_sort([34, 2, 10, -9]))
