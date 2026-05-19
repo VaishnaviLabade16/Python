76a2549f7736b85092b1824816dffeef55930aa9
VaishnaviLabade16 <vaishnavilabade7@gmail.com>
2026-05-19 18:18:35 +0530

    Add files via upload

---
+++ Bogo_Sort.py
@@ -0,0 +1,11 @@
+import random
+
+def is_sorted(arr):
+    return all(arr[i] <= arr[i+1] for i in range(len(arr)-1))
+
+def bogo_sort(arr):
+    while not is_sorted(arr):
+        random.shuffle(arr)
+    return arr
+
+print(bogo_sort([3, 2, 1]))
