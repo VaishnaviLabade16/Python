5a5784819f81a5734d74f02c1590449e4a0493b5
VaishnaviLabade16 <vaishnavilabade7@gmail.com>
2026-05-22 20:33:11 +0530

    Add files via upload

---
+++ Cycle_Sort.py
@@ -0,0 +1,34 @@
+def cycle_sort(arr):
+    n = len(arr)
+
+    for cycle_start in range(n - 1):
+        item = arr[cycle_start]
+
+        pos = cycle_start
+        for i in range(cycle_start + 1, n):
+            if arr[i] < item:
+                pos += 1
+
+        if pos == cycle_start:
+            continue
+
+        while item == arr[pos]:
+            pos += 1
+
+        arr[pos], item = item, arr[pos]
+
+        while pos != cycle_start:
+            pos = cycle_start
+
+            for i in range(cycle_start + 1, n):
+                if arr[i] < item:
+                    pos += 1
+
+            while item == arr[pos]:
+                pos += 1
+
+            arr[pos], item = item, arr[pos]
+
+    return arr
+
+print(cycle_sort([1, 8, 3, 9, 10, 10, 2, 4]))
