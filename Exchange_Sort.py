91f7d4e90f009cd08b6938c3b91bb2efcf9a1e2e
VaishnaviLabade16 <vaishnavilabade7@gmail.com>
2026-06-23 13:36:50 +0530

    Add files via upload

---
+++ Exchange_Sort.py
@@ -0,0 +1,11 @@
+def exchange_sort(arr):
+    n = len(arr)
+
+    for i in range(n - 1):
+        for j in range(i + 1, n):
+            if arr[i] > arr[j]:
+                arr[i], arr[j] = arr[j], arr[i]
+
+    return arr
+
+print(exchange_sort([5, 4, 3, 2, 1]))
